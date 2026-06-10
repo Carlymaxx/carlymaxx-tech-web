7515484b28bbb7d5ba166685535c9a2d58032e03
Carlymaxx <caillmashariacalvince@gmail.com>
2026-06-10 17:03:00 +0300

    Add /carlymaxx short route alongside /about-carly-maxx

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about-carly-maxx" element={<CarlyMaxx />} />
+          <Route path="/carlymaxx" element={<CarlyMaxx />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
